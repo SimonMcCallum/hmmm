70a9925e5d9412810de78d5957ae94d9d7066433
Simon McCallum <simon.mccallum@gmail.com>
2020-09-10 12:51:11 +1200

    after part 2 of the tutorial #1

---
+++ Source/hmmm/hmmmGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "hmmmGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AhmmmGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AhmmmGameMode();
+};
+
+
+
